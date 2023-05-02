6c7ef8d5072a1e8a2421538fb4adf63092f741cd
21ITL130Pradeep <125948189+21ITL130Pradeep@users.noreply.github.com>
2023-05-02 11:45:52 +0530

    Add files via upload

---
+++ post.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const querystring = require('querystring')
+var qs, name, passwd
+const port = 4000
+http.createServer((req, res) => {
+    var value = ""
+    req.on('data', (ch) => {
+        console.log(ch)
+        value += ch
+        console.log("readable data " + value)
+    });
+    req.on('end', () => {
+        qs = querystring.parse(value)
+        console.log(qs)
+        name = qs['username']
+        passwd = qs['password']
+        res.write("Welcome " + name + " your Password is " + passwd)
+        res.end()
+    });
+}).listen(port, (error) => {
+    if (error) {
+        console.log("error occures")
+    }
+    else {
+        console.log("server listening on " + port)
+    }
+});
+
